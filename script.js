6de3400a1a159d154a8a4f4d3f5da5cd30c7e45c
Swatantra-66 <maverickswatantra@gmail.com>
2024-12-22 20:06:50 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = "Swatantra";
+let email = "maverickswatantra@gmail.com";
+let password = "MaVeRicK15$";
+let phone = 9151356121;
+let is_loggedInFrom_google = false;
+let loggedInStatus = true;
+
+console.log(`
+    username is : ${name},
+    user email is : ${email},
+    password is : ${password},
+    Phone No is : ${phone},
+    loggedInFromGoogle : ${is_loggedInFrom_google},
+    loggin succes : ${loggedInStatus}
+    `);
